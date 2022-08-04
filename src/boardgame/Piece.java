e76949dc200b2aef93d2d30e64f0500b16112b86
Harry <harrywendson@gmail.com>
2022-08-03 21:07:39 -0300

    Melhorando a impressão do tabuleiro

--- src/boardgame/Piece.java
+++ src/boardgame/Piece.java
@@ -2,16 +2,16 @@ package boardgame;
 
 public class Piece {
 
-		protected Position position;
-		private Board board;
-		
-		public Piece(Board board) {
-			this.board = board;
-			position = null;
-		}
+	protected Position position;
+	private Board board;
 
-		protected Board getBoard() {
-			return board;
-		}
-		
+	public Piece(Board board) {
+		this.board = board;
+		position = null;
+	}
+
+	protected Board getBoard() {
+		return board;
+	}
+
 }
